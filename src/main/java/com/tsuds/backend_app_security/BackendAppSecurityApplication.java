edbb0dfe6eae9e4699014c5bb06a5da949781d81
Johnny Garcia <johnnygar98@hotmail.com>
2024-07-29 20:57:03 -0500

    initial commit

---
+++ src/main/java/com/tsuds/backend_app_security/BackendAppSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tsuds.backend_app_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAppSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsuds/backend_app_security/BackendAppSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsuds.backend_app_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

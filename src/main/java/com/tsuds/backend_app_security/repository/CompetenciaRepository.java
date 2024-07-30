9bc953625f60264b97bee635be1b2b32dc086ca5
armando2197 <47511883+armando2197@users.noreply.github.com>
2024-07-29 21:46:01 -0500

    Create CompetenciaRepository.java

---
+++ src/main/java/com/tsuds/backend_app_security/repository/CompetenciaRepository.java
@@ -0,0 +1,9 @@
+package com.tsuds.backend_app_security.repository;
+
+import com.tsuds.backend_app_security.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompetenciaRepository extends JpaRepository<Competencia, Integer> {
+}

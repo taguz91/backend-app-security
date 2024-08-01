ee374dc8c99ccac026b86a30f53db0c5ba060cce
armando2197 <47511883+armando2197@users.noreply.github.com>
2024-08-01 18:38:33 -0500

    Update FacturaDto.java

--- src/main/java/com/tsuds/backend_app_security/dto/FacturaDto.java
+++ src/main/java/com/tsuds/backend_app_security/dto/FacturaDto.java
@@ -1,1 +1,26 @@
 
+package com.tsuds.backend_app_security.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class FacturaDto {
+
+    @NotBlank(message = "El ruc es requerido para crear la factura")
+      private String ruc;
+
+ @NotBlank(message = "la persona es requerido para crear la factura")
+    private int idPersona;
+  
+ @NotBlank(message = "la recha es requerida crear la factura")
+    private Date fecha;
+
+    @NotBlank(message = "El tipo de pago  es requerido para crear la factura")
+    private int idTipoPago;
+  
+ @NotBlank(message = "el descuento es requerido para crear la factura")
+    private double descuento;
+  
+   @NotBlank(message = "El total es requerido para crear la factura")
+    private double total;
+
+}

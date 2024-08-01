2a0a10f1233c799e795b25f5d3d80eed921282a0
armando2197 <47511883+armando2197@users.noreply.github.com>
2024-08-01 18:54:32 -0500

    Update PersonaDto.java

--- src/main/java/com/tsuds/backend_app_security/dto/PersonaDto.java
+++ src/main/java/com/tsuds/backend_app_security/dto/PersonaDto.java
@@ -1,1 +1,27 @@
 
+package com.tsuds.backend_app_security.dto;
+
+import com.tsuds.backend_app_security.models.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class PersonaDto {
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+      private String nombre;
+  
+  @NotBlank(message = "El apellido de usuario es obligatorio")
+    private String apellido;
+  
+  @NotBlank(message = "El dni de usuario es obligatorio")
+    private String dni;
+  
+  @NotBlank(message = "El celular de usuario es obligatorio")
+    private String celular;
+  
+  @NotBlank(message = "El correo electronico de usuario es obligatorio")
+    private String correo;
+
+    }
+}

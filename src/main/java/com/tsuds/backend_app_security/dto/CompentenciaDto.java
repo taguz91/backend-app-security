4ac9aff68ba1514ba7985fb9d8e53c2904c0e1be
armando2197 <47511883+armando2197@users.noreply.github.com>
2024-08-01 18:31:10 -0500

    Update CompentenciaDto.java

--- src/main/java/com/tsuds/backend_app_security/dto/CompentenciaDto.java
+++ src/main/java/com/tsuds/backend_app_security/dto/CompentenciaDto.java
@@ -1,1 +1,12 @@
+package com.tsuds.backend_app_security.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
+public class CompetenciaDto {
+
+    @NotBlank(message = "El nombre es requerido")
+  private String nombre;
+  @NotBlank(message = "la descripcion es requerida")
+    private String descripcion;
+
+}

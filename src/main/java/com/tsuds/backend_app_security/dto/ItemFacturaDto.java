215f4bec8b57f07a73e7ac01dead579ac80b5413
armando2197 <47511883+armando2197@users.noreply.github.com>
2024-08-01 18:48:22 -0500

    Update ItemFacturaDto.java

--- src/main/java/com/tsuds/backend_app_security/dto/ItemFacturaDto.java
+++ src/main/java/com/tsuds/backend_app_security/dto/ItemFacturaDto.java
@@ -1,1 +1,26 @@
 
+package com.tsuds.backend_app_security.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class FacturaDto {
+
+    @NotBlank(message = "El item de la factura es requerido para crear la factura")
+ private int ItemFacturaDto;
+
+    @NotBlank(message = "el identificador de la factura es requerido para credar la factura")
+    private int IdFactura;
+
+    @NotBlank(message = "El identificador del producto es requerido para crear la factura")
+    private Producto IdProducto;
+  
+    @NotBlank(message = "la cantidad de uvas es requerido para crear la factura")
+    private int cantidad;
+  
+      @NotBlank(message = "El precio de la factura es requerido para crear la factura")
+    private double precio;
+  
+      @NotBlank(message = "El subtotanl de la factura es requerido para crear la factura")
+    private double subtotal;
+
+}

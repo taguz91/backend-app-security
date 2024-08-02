af895d3dc4e6d2715ea34a8224aa1a0c09274238
armando2197 <47511883+armando2197@users.noreply.github.com>
2024-08-01 19:15:28 -0500

    Update ProveedoresDto.java

--- src/main/java/com/tsuds/backend_app_security/dto/ProveedoresDto.java
+++ src/main/java/com/tsuds/backend_app_security/dto/ProveedoresDto.java
@@ -1,1 +1,22 @@
 
+
+package com.tsuds.backend_app_security.dto;
+
+import com.tsuds.backend_app_security.models.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ProveedoresDto {
+
+    @NotBlank(message = "El nombre el proveedor es obligatorio")
+    private String nombre;
+  @NotBlank(message = "El ruc el proveedor es obligatorio")
+    private String ruc;
+  @NotBlank(message = "El relefono de contacto del proveedor es obligatorio")
+    private String telefono;
+  @NotBlank(message = "El pais de recidencia del proveedor es obligatorio")
+    private String pais;
+  
+    }
+}

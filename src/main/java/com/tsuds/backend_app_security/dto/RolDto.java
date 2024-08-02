739a36dc91116b818adb11763a597f5eb2242cc7
armando2197 <47511883+armando2197@users.noreply.github.com>
2024-08-01 19:18:01 -0500

    Update RolDto.java

--- src/main/java/com/tsuds/backend_app_security/dto/RolDto.java
+++ src/main/java/com/tsuds/backend_app_security/dto/RolDto.java
@@ -1,1 +1,21 @@
 
+
+package com.tsuds.backend_app_security.dto;
+
+import com.tsuds.backend_app_security.models.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RolDto {
+
+    @NotBlank(message = "El nombre del rol es obligatorio")
+     private String rol;
+
+      @NotBlank(message = "El estado del rol es obligatorio")
+    private boolean estado;
+  
+
+
+    }
+}

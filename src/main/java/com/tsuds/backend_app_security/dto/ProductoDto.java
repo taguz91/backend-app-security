92251324996c566f893d820971cc980613bb8bad
armando2197 <47511883+armando2197@users.noreply.github.com>
2024-08-01 19:13:10 -0500

    Update ProductoDto.java

--- src/main/java/com/tsuds/backend_app_security/dto/ProductoDto.java
+++ src/main/java/com/tsuds/backend_app_security/dto/ProductoDto.java
@@ -1,1 +1,29 @@
 
+
+package com.tsuds.backend_app_security.dto;
+
+import com.tsuds.backend_app_security.models.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ProductoDto {
+
+    @NotBlank(message = "El stock del producto es obligatorio")
+    private int stock;
+    @NotBlank(message = "El precion unitario del producto es obligatorio")
+    private double precioUnitario;
+
+    @NotBlank(message = "las unidades del producto es obligatorio")
+    private String unidad;
+  
+    @NotBlank(message = "la clasificacion del producto es obligatorio")
+    private Clasificacion clasificacion;
+
+    @NotBlank(message = "El proveedor del producto es obligatorio")
+    private Proveedores proveedor;
+
+    @NotBlank(message = "El iva que controla el producto es obligatorio")
+    private boolean iva;
+    }
+}
